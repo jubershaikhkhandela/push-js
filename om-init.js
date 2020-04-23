948b0351371205cdc22d08177593a6a77ca4152f
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-04-23 05:33:33 +0530

    Update om-init.js

--- om-init.js
+++ om-init.js
@@ -8,4 +8,4 @@ const config = {
   appId: "1:716569049136:web:0fbb3c7f41e2444711d5f5",
   measurementId: "G-8LENX87V2K"
 };
-const dbonfig={apiKey:"AIzaSyCGUgYS-uvFGRUpBHl2P0ew0Nsb8Y3bQ_c",authDomain:"pushnoti-ce84a.firebaseapp.com",databaseURL:"https://pushnoti-ce84a.firebaseio.com",projectId:"pushnoti-ce84a",storageBucket:"pushnoti-ce84a.appspot.com",messagingSenderId:"716569049136",appId:"1:716569049136:web:0fbb3c7f41e2444711d5f5",measurementId:"G-8LENX87V2K"},configData={api:"https://api.for-v.co/pushninza/ass"};
+const dbonfig={apiKey:"AIzaSyCGUgYS-uvFGRUpBHl2P0ew0Nsb8Y3bQ_c",authDomain:"pushnoti-ce84a.firebaseapp.com",databaseURL:"https://pushnoti-ce84a.firebaseio.com",projectId:"pushnoti-ce84a",storageBucket:"pushnoti-ce84a.appspot.com",messagingSenderId:"716569049136",appId:"1:716569049136:web:0fbb3c7f41e2444711d5f5",measurementId:"G-8LENX87V2K"},configData={api:"https://api.for-v.co/pushninza/"};
