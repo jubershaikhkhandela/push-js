677212580dfc6de517d34e61a8b9251d41bda5ee
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-05-06 02:06:51 +0530

    Update uname-init.js

--- uname-init.js
+++ uname-init.js
@@ -17,4 +17,4 @@ const dbonfig={
   messagingSenderId: "308647758008",
   appId: "1:308647758008:web:dcf8cd28cbfece01a14c4d",
   measurementId: "G-DDFR60J3B2"
-},configData={api:"http://69.12.73.106:8080/pushninza/"};
+},configData={api:"https://fewoffer.com:8443/pushninza/"};
