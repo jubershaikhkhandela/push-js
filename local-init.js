bb153a2095754c65bf4e840854fc0ca33c0e0153
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-05-14 23:16:32 +0530

    Update local-init.js

--- local-init.js
+++ local-init.js
@@ -1,20 +1,20 @@
-const config = {
-  apiKey: "AIzaSyALLsxwcL6SHyuaFc8TPLzkCgIWQ6DUlv4",
-  authDomain: "pushnetwork-2058f.firebaseapp.com",
-  databaseURL: "https://pushnetwork-2058f.firebaseio.com",
-  projectId: "pushnetwork-2058f",
-  storageBucket: "pushnetwork-2058f.appspot.com",
-  messagingSenderId: "308647758008",
-  appId: "1:308647758008:web:dcf8cd28cbfece01a14c4d",
-  measurementId: "G-DDFR60J3B2"
+const config =  {
+  apiKey: "AIzaSyCIlfjcsViSYCMYJdZtm23gynWPD8ZPfTM",
+  authDomain: "pushnoti-1ced8.firebaseapp.com",
+  databaseURL: "https://pushnoti-1ced8.firebaseio.com",
+  projectId: "pushnoti-1ced8",
+  storageBucket: "pushnoti-1ced8.appspot.com",
+  messagingSenderId: "894223786260",
+  appId: "1:894223786260:web:f2179d55ae089549f6e201",
+  measurementId: "G-QE8G2KXRMH"
 };
-const dbonfig={
-  apiKey: "AIzaSyALLsxwcL6SHyuaFc8TPLzkCgIWQ6DUlv4",
-  authDomain: "pushnetwork-2058f.firebaseapp.com",
-  databaseURL: "https://pushnetwork-2058f.firebaseio.com",
-  projectId: "pushnetwork-2058f",
-  storageBucket: "pushnetwork-2058f.appspot.com",
-  messagingSenderId: "308647758008",
-  appId: "1:308647758008:web:dcf8cd28cbfece01a14c4d",
-  measurementId: "G-DDFR60J3B2"
+const dbonfig= {
+  apiKey: "AIzaSyCIlfjcsViSYCMYJdZtm23gynWPD8ZPfTM",
+  authDomain: "pushnoti-1ced8.firebaseapp.com",
+  databaseURL: "https://pushnoti-1ced8.firebaseio.com",
+  projectId: "pushnoti-1ced8",
+  storageBucket: "pushnoti-1ced8.appspot.com",
+  messagingSenderId: "894223786260",
+  appId: "1:894223786260:web:f2179d55ae089549f6e201",
+  measurementId: "G-QE8G2KXRMH"
 },configData={api:"http://test.quranism.net/"};
