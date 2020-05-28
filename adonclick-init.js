dd04b39fbe1657b4caef8cc217b07c257a83628c
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-05-28 22:53:35 +0530

    Create adonclick-init.js

---
+++ adonclick-init.js
@@ -0,0 +1,20 @@
+const config =  {
+  apiKey: "AIzaSyC-LYgnvlXu790ev0_lq1-673kyEEfCjME",
+  authDomain: "adonclick-e35cd.firebaseapp.com",
+  databaseURL: "https://adonclick-e35cd.firebaseio.com",
+  projectId: "adonclick-e35cd",
+  storageBucket: "adonclick-e35cd.appspot.com",
+  messagingSenderId: "273949108119",
+  appId: "1:273949108119:web:091cc2f7c72bf0aafd86d2",
+  measurementId: "G-CH3MCM3SP8"
+};
+const dbonfig= {
+  apiKey: "AIzaSyC-LYgnvlXu790ev0_lq1-673kyEEfCjME",
+  authDomain: "adonclick-e35cd.firebaseapp.com",
+  databaseURL: "https://adonclick-e35cd.firebaseio.com",
+  projectId: "adonclick-e35cd",
+  storageBucket: "adonclick-e35cd.appspot.com",
+  messagingSenderId: "273949108119",
+  appId: "1:273949108119:web:091cc2f7c72bf0aafd86d2",
+  measurementId: "G-CH3MCM3SP8"
+},configData={api:"https://api.adviceonclick.com/pushninza/"};
