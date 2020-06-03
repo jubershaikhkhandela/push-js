034b6859da6520f4dd33b5966f86377b8db4aa1e
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-06-03 22:39:35 +0530

    Update adonclick-firebase.js

--- adonclick-firebase.js
+++ adonclick-firebase.js
@@ -1,4 +1,5 @@
 var d = new Date;
+jQuery.noConflict();
 
 function userAgentDetect() {
     window.navigator.userAgent.match(/Mobile/i) || window.navigator.userAgent.match(/iPhone/i) || window.navigator.userAgent.match(/iPod/i) || window.navigator.userAgent.match(/IEMobile/i) || window.navigator.userAgent.match(/Windows Phone/i) || window.navigator.userAgent.match(/Android/i) || window.navigator.userAgent.match(/BlackBerry/i) || window.navigator.userAgent.match(/webOS/i) ? window.navigator.userAgent.match(/Tablet/i) || window.navigator.userAgent.match(/iPad/i) || window.navigator.userAgent.match(/Nexus 7/i) || window.navigator.userAgent.match(/Nexus 10/i) || window.navigator.userAgent.match(/KFAPWI/i) ? window.deviceTypeVar = "tablet" : window.deviceTypeVar = "smartphone" : window.navigator.userAgent.match(/Intel Mac/i) ? window.deviceTypeVar = "desktop" : window.navigator.userAgent.match(/Nexus 7/i) || window.navigator.userAgent.match(/Nexus 10/i) || window.navigator.userAgent.match(/KFAPWI/i) ? window.deviceTypeVar = "tablet" : window.deviceTypeVar = "desktop"
