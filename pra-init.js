293801cd84f1289b095c8991193a1545ff64446f
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-08-09 01:10:04 +0530

    Create pra-init.js

---
+++ pra-init.js
@@ -0,0 +1,18 @@
+const config =  {
+  apiKey: "AIzaSyCidQf8Bu9vWREgJmM-juzVYscHSpCZrjM",
+  authDomain: "push-99813.firebaseapp.com",
+  databaseURL: "https://push-99813.firebaseio.com",
+  projectId: "push-99813",
+  storageBucket: "push-99813.appspot.com",
+  messagingSenderId: "713916285339",
+  appId: "1:713916285339:web:056a7b25dd20446581a13e"
+};
+const dbonfig= {
+  apiKey: "AIzaSyCidQf8Bu9vWREgJmM-juzVYscHSpCZrjM",
+  authDomain: "push-99813.firebaseapp.com",
+  databaseURL: "https://push-99813.firebaseio.com",
+  projectId: "push-99813",
+  storageBucket: "push-99813.appspot.com",
+  messagingSenderId: "713916285339",
+  appId: "1:713916285339:web:056a7b25dd20446581a13e"
+},configData={api:"https://api.mahadevpush.xyz/pushninza/"};
