a157510a640f617ddb4f48a46532193e416af5ee
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-08-16 23:24:25 +0530

    Create prakash-init.js

---
+++ prakash-init.js
@@ -0,0 +1,18 @@
+const config = {
+  apiKey: "AIzaSyCCxOPgGQ9MZA9RZ4wVh76dQZZC6-eybA8",
+  authDomain: "push-95cf3.firebaseapp.com",
+  databaseURL: "https://push-95cf3.firebaseio.com",
+  projectId: "push-95cf3",
+  storageBucket: "push-95cf3.appspot.com",
+  messagingSenderId: "44237560877",
+  appId: "1:44237560877:web:18e9e3443463a971d22ca4"
+};
+const dbonfig={
+  apiKey: "AIzaSyCCxOPgGQ9MZA9RZ4wVh76dQZZC6-eybA8",
+  authDomain: "push-95cf3.firebaseapp.com",
+  databaseURL: "https://push-95cf3.firebaseio.com",
+  projectId: "push-95cf3",
+  storageBucket: "push-95cf3.appspot.com",
+  messagingSenderId: "44237560877",
+  appId: "1:44237560877:web:18e9e3443463a971d22ca4"
+},configData={api:"https://api.happy-indian.com/pushninza/"};
