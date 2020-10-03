e5317a9a764872a7589d43249bc5272d600c1f02
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-10-03 13:21:11 +0530

    Create atul-init.js

---
+++ atul-init.js
@@ -0,0 +1,18 @@
+const config = {
+   apiKey: "AIzaSyBA7V369Wr4JSA3Nzeyxk-fyYq2wP3vS5w",
+  authDomain: "push-e3e0c.firebaseapp.com",
+  databaseURL: "https://push-e3e0c.firebaseio.com",
+  projectId: "push-e3e0c",
+  storageBucket: "push-e3e0c.appspot.com",
+  messagingSenderId: "802741052114",
+  appId: "1:802741052114:web:2cfb86f0cef829e624c30b",
+  measurementId: "G-C4K7Z6C4DT"
+};
+const dbonfig={ apiKey: "AIzaSyBA7V369Wr4JSA3Nzeyxk-fyYq2wP3vS5w",
+  authDomain: "push-e3e0c.firebaseapp.com",
+  databaseURL: "https://push-e3e0c.firebaseio.com",
+  projectId: "push-e3e0c",
+  storageBucket: "push-e3e0c.appspot.com",
+  messagingSenderId: "802741052114",
+  appId: "1:802741052114:web:2cfb86f0cef829e624c30b",
+  measurementId: "G-C4K7Z6C4DT"},configData={api:"https://api.pushbun.com/pushninza/"};
