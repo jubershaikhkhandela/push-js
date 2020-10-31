145791d3856f7b5fc024ed5a092564a36ee83187
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-10-31 14:59:13 +0530

    Create nikhil-init.js

---
+++ nikhil-init.js
@@ -0,0 +1,18 @@
+const config = {
+    apiKey: "AIzaSyCZX6ZGxYb4oRV6tend2r__p2kFmRKoG9c",
+  authDomain: "push-6dde9.firebaseapp.com",
+  databaseURL: "https://push-6dde9.firebaseio.com",
+  projectId: "push-6dde9",
+  storageBucket: "push-6dde9.appspot.com",
+  messagingSenderId: "774986388186",
+  appId: "1:774986388186:web:a6028e1752c3db5650cbe3",
+  measurementId: "G-ZWXRB57TRX"
+};
+const dbonfig={  apiKey: "AIzaSyCZX6ZGxYb4oRV6tend2r__p2kFmRKoG9c",
+  authDomain: "push-6dde9.firebaseapp.com",
+  databaseURL: "https://push-6dde9.firebaseio.com",
+  projectId: "push-6dde9",
+  storageBucket: "push-6dde9.appspot.com",
+  messagingSenderId: "774986388186",
+  appId: "1:774986388186:web:a6028e1752c3db5650cbe3",
+  measurementId: "G-ZWXRB57TRX"},configData={api:"https://royal.quranism.net/pushninza/"};
