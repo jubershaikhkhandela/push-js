47eae1c061275199c01192a8bb4ce76c4bd7e160
aashiqui <40266375+aashiqui@users.noreply.github.com>
2020-11-09 21:20:54 +0530

    Create faheem-init.js

---
+++ faheem-init.js
@@ -0,0 +1,20 @@
+const config = {
+  apiKey: "AIzaSyA2sID_RL9EkIkmC-YE8QsO_sH_j5k5gKs",
+  authDomain: "push-c4876.firebaseapp.com",
+  databaseURL: "https://push-c4876.firebaseio.com",
+  projectId: "push-c4876",
+  storageBucket: "push-c4876.appspot.com",
+  messagingSenderId: "179659269129",
+  appId: "1:179659269129:web:684fb31c8ff961d05b44d8",
+  measurementId: "G-FEFJRM897P"
+};
+const dbonfig={
+  apiKey: "AIzaSyA2sID_RL9EkIkmC-YE8QsO_sH_j5k5gKs",
+  authDomain: "push-c4876.firebaseapp.com",
+  databaseURL: "https://push-c4876.firebaseio.com",
+  projectId: "push-c4876",
+  storageBucket: "push-c4876.appspot.com",
+  messagingSenderId: "179659269129",
+  appId: "1:179659269129:web:684fb31c8ff961d05b44d8",
+  measurementId: "G-FEFJRM897P"
+},configData={api:"https://diamondpush.com:8880/pushninza/"};
