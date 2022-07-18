bef545423bf8c6de798e269e52e9c7ba312fac70
aashiqui <40266375+aashiqui@users.noreply.github.com>
2022-07-18 11:43:01 +0530

    Create vishal-init2.js

---
+++ vishal-init2.js
@@ -0,0 +1,18 @@
+const config = {
+  apiKey: "AIzaSyDUI7eJmH_OXLygMH7EAV6Yed97s7dfAE4",
+  authDomain: "push-cff8e.firebaseapp.com",
+  projectId: "push-cff8e",
+  storageBucket: "push-cff8e.appspot.com",
+  messagingSenderId: "325854086582",
+  appId: "1:325854086582:web:60bcd3dcd71d307c47ed17",
+  measurementId: "G-JKB3LEERN5"
+};
+const dbonfig = {
+  apiKey: "AIzaSyDUI7eJmH_OXLygMH7EAV6Yed97s7dfAE4",
+  authDomain: "push-cff8e.firebaseapp.com",
+  projectId: "push-cff8e",
+  storageBucket: "push-cff8e.appspot.com",
+  messagingSenderId: "325854086582",
+  appId: "1:325854086582:web:60bcd3dcd71d307c47ed17",
+  measurementId: "G-JKB3LEERN5"
+},configData={api:"https://api.vishalchaturvedi.com/pushninza/"};
