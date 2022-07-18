c2ba575bc273e105685d6e5c02f54968055f6e60
aashiqui <40266375+aashiqui@users.noreply.github.com>
2022-07-18 11:28:08 +0530

    Update vishal-init.js

--- vishal-init.js
+++ vishal-init.js
@@ -6,7 +6,7 @@ const config = {
   messagingSenderId: "325854086582",
   appId: "1:325854086582:web:60bcd3dcd71d307c47ed17",
   measurementId: "G-JKB3LEERN5"
-};;
+};
 const dbonfig = {
   apiKey: "AIzaSyDUI7eJmH_OXLygMH7EAV6Yed97s7dfAE4",
   authDomain: "push-cff8e.firebaseapp.com",
