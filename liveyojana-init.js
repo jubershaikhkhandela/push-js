4a559c7d6168574057406d35d5dc68abe3bdba33
aashiqui <40266375+aashiqui@users.noreply.github.com>
2023-02-18 17:41:55 +0530

    Create liveyojana-init.js

---
+++ liveyojana-init.js
@@ -0,0 +1,16 @@
+const config = {
+  apiKey: "AIzaSyAJMtDhFwzPdjCQvbOY0XgEA1k52YwybHE",
+  authDomain: "liveyojana-328f3.firebaseapp.com",
+  projectId: "liveyojana-328f3",
+  storageBucket: "liveyojana-328f3.appspot.com",
+  messagingSenderId: "967494665113",
+  appId: "1:967494665113:web:ba485358d5dffb9fb78908"
+};
+const dbonfig = {
+  apiKey: "AIzaSyAJMtDhFwzPdjCQvbOY0XgEA1k52YwybHE",
+  authDomain: "liveyojana-328f3.firebaseapp.com",
+  projectId: "liveyojana-328f3",
+  storageBucket: "liveyojana-328f3.appspot.com",
+  messagingSenderId: "967494665113",
+  appId: "1:967494665113:web:ba485358d5dffb9fb78908"
+},configData={api:"https://portal-api.sarkariyojnaa.com/pushninza/"};
