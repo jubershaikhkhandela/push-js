6e10b8828275c39d4bf329243b759ff2a2cd7b81
aashiqui <40266375+aashiqui@users.noreply.github.com>
2023-02-18 17:43:23 +0530

    Create csc-digital-seva-init.js

---
+++ csc-digital-seva-init.js
@@ -0,0 +1,16 @@
+const config = {
+  apiKey: "AIzaSyBUAXgVp1JLiLDGaoy39B6QXvqlewha6PQ",
+  authDomain: "csc-digital-seva-93a42.firebaseapp.com",
+  projectId: "csc-digital-seva-93a42",
+  storageBucket: "csc-digital-seva-93a42.appspot.com",
+  messagingSenderId: "894131953211",
+  appId: "1:894131953211:web:9e6a119ef68731e915461e"
+};
+const dbonfig = {
+  apiKey: "AIzaSyBUAXgVp1JLiLDGaoy39B6QXvqlewha6PQ",
+  authDomain: "csc-digital-seva-93a42.firebaseapp.com",
+  projectId: "csc-digital-seva-93a42",
+  storageBucket: "csc-digital-seva-93a42.appspot.com",
+  messagingSenderId: "894131953211",
+  appId: "1:894131953211:web:9e6a119ef68731e915461e"
+},configData={api:"https://portal-api.sarkariyojnaa.com/pushninza/"};
