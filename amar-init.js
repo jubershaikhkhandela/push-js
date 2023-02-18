0c3f5f2bc8c990789493250df28ce9258bc1c58c
aashiqui <40266375+aashiqui@users.noreply.github.com>
2023-02-18 17:40:39 +0530

    Create amar-init.js

---
+++ amar-init.js
@@ -0,0 +1,16 @@
+const config = {
+  apiKey: "AIzaSyBGlG0qYsiiowyLsLs6MV_BMXb2Ekmtuv4",
+  authDomain: "sarkari-yojana-new.firebaseapp.com",
+  projectId: "sarkari-yojana-new",
+  storageBucket: "sarkari-yojana-new.appspot.com",
+  messagingSenderId: "532377717173",
+  appId: "1:532377717173:web:495400938abb1685c131fc"
+};
+const dbonfig = {
+  apiKey: "AIzaSyBGlG0qYsiiowyLsLs6MV_BMXb2Ekmtuv4",
+  authDomain: "sarkari-yojana-new.firebaseapp.com",
+  projectId: "sarkari-yojana-new",
+  storageBucket: "sarkari-yojana-new.appspot.com",
+  messagingSenderId: "532377717173",
+  appId: "1:532377717173:web:495400938abb1685c131fc"
+},configData={api:"https://portal-api.sarkariyojnaa.com/pushninza/"};
